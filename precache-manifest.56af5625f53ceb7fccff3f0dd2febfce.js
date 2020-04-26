3fa858cbd9bb98e52de60fa31ea92806a53ec3b2
Aykut <aykut0171@gmail.com>
2020-04-27 03:20:38 +1000

    Updates

---
+++ precache-manifest.56af5625f53ceb7fccff3f0dd2febfce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9585af9d39eb7444965f93b98c223fae",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "9c020a8bab948966f893",
+    "url": "/myapp/static/css/main.72e83d7b.chunk.css"
+  },
+  {
+    "revision": "2bb8443f49c2d51f10cc",
+    "url": "/myapp/static/js/2.9caf2576.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myapp/static/js/2.9caf2576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c020a8bab948966f893",
+    "url": "/myapp/static/js/main.a3e3f0ac.chunk.js"
+  },
+  {
+    "revision": "7c157e24c57b3c3aa05a",
+    "url": "/myapp/static/js/runtime-main.6d446693.js"
+  },
+  {
+    "revision": "f6082ca6324bb65be055be3d6589148d",
+    "url": "/myapp/static/media/about.f6082ca6.jpg"
+  },
+  {
+    "revision": "d53fed8c61ca8d40df2409f1088ef572",
+    "url": "/myapp/static/media/banner.d53fed8c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
